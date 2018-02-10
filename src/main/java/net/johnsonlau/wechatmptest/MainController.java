29f0f686a9c9e67c19fe66e53f836104453155c5
Johnson Lau <gatement@gmail.com>
2018-02-11 01:07:44 +0800

    go

--- src/main/java/net/johnsonlau/wechatmptest/MainController.java
+++ src/main/java/net/johnsonlau/wechatmptest/MainController.java
@@ -1,15 +1,28 @@
 package net.johnsonlau.wechatmptest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class MainController {
+import lombok.extern.slf4j.Slf4j;
 
-	@RequestMapping(path="/wechat", method=RequestMethod.GET)
-	public String wechat() {
-		return "hello world2";
+@RestController
+@Slf4j
+public class MainController {
+	private static String TOKEN = "123456";
+
+	@GetMapping("/wechat")
+	public String wechat(@RequestParam("signature") String signature, @RequestParam("timestamp") String timestamp,
+			@RequestParam("nonce") String nonce, @RequestParam("echostr") String echostr) {
+		if (checkSignature(signature, timestamp, nonce)) {
+			return echostr;
+		} else {
+			return "";
+		}
+	}
+
+	private boolean checkSignature(String signature, String timestamp, String nonce) {
+		return true;
 	}
 
 }
