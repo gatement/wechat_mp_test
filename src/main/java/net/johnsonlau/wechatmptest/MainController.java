f50bd7028c4fa4582204932bca5375978efcc796
Johnson Lau <gatement@gmail.com>
2018-02-11 01:09:37 +0800

    go

--- src/main/java/net/johnsonlau/wechatmptest/MainController.java
+++ src/main/java/net/johnsonlau/wechatmptest/MainController.java
@@ -14,6 +14,7 @@ public class MainController {
 	@GetMapping("/wechat")
 	public String wechat(@RequestParam("signature") String signature, @RequestParam("timestamp") String timestamp,
 			@RequestParam("nonce") String nonce, @RequestParam("echostr") String echostr) {
+		log.info("signature: {}, timestamp: {}, nonce: {}, echostr: {}", signature, timestamp, nonce, echostr);
 		if (checkSignature(signature, timestamp, nonce)) {
 			return echostr;
 		} else {
@@ -22,7 +23,9 @@ public class MainController {
 	}
 
 	private boolean checkSignature(String signature, String timestamp, String nonce) {
-		return true;
+		boolean result = false;
+		log.info("verify result: {}", result);
+		return result;
 	}
 
 }
