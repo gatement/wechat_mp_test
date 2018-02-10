2bf71f4ced01a6bfacfa3933422c2e2cc40f34cf
Johnson Lau <gatement@gmail.com>
2018-02-11 00:50:00 +0800

    go

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+scp target/wechat_mp_test-0.1.0-SNAPSHOT.jar root@jserver1:/root/downloads/
--- src/main/java/net/johnsonlau/wechatmptest/MainController.java
+++ src/main/java/net/johnsonlau/wechatmptest/MainController.java
@@ -1,13 +1,14 @@
 package net.johnsonlau.wechatmptest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-	@RequestMapping("/hello")
-	public String hello() {
+	@RequestMapping(path="/wechat", method=RequestMethod.GET)
+	public String wechat() {
 		return "hello world2";
 	}
 
